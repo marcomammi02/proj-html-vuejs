938cafb1a19d4dc246634e095ee4e39538daf36f
marcomammi02 <marcomammi2002@gmail.com>
2022-12-05 21:55:55 +0100

    Ticket 1

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <HeaderPage />
+  </div>
+</template>
+
+<script>
+import HeaderPage from '@/components/HeaderPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderPage,
+  },
+};
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+
+</style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,95 @@
+<template>
+  <header>
+    <div class="navbar">
+      <button>ORDER ONLINE</button>
+      <ul>
+        <li
+          v-for="(objMenu, i) in menuHeader"
+          :key="i"
+        >
+          <a
+            v-if="(objMenu[i] == 0)"
+            href="#"
+          >
+            {{ objMenu.toUpperCase() }}</a>
+        </li>
+      </ul>
+      <div class="btns">
+        <div class="btn cart">
+          CART
+        </div>
+        <div class="btn search">
+          SEARCH
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+  data() {
+    return {
+      menuHeader: [
+        'home',
+        'pages',
+        'menu',
+        'event',
+        'blog',
+        'landing',
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  header {
+    background-image: url('@/img/background-header.png');
+    padding-bottom: 25rem;
+  }
+  .navbar {
+    max-width: 900px;
+    margin: 0 auto;
+    padding-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    font-size: .7rem;
+  }
+
+  button {
+    color: #ffffff;
+    background-color: #d2401e;
+    border: none;
+    padding: .7rem 1.5rem;
+    font-size: .7rem;
+  }
+
+  ul {
+    display: flex;
+  }
+
+  li {
+    list-style: none;
+    margin: 10px;
+  }
+
+  li:nth-child(3) {
+    margin-right: 5rem;
+  }
+  li:nth-child(4) {
+    margin-left: 5rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: #ffffff;
+  }
+
+  .btns {
+    display: flex;
+    align-items: center;
+    color: #ffffff;
+  }
+</style>
